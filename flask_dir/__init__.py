6fff474960df24d552566da4b8ed3e94f1f62e7d
DenisPALD <masgutovmihail@gmail.com>
2021-07-13 14:18:29 +0500

    add __init__ file, more laconic launch

---
+++ flask_dir/__init__.py
@@ -0,0 +1,6 @@
+from .app import app
+from .view import *
+from .papers.blueprint import papers as blueprint_paper
+
+
+app.register_blueprint(blueprint_paper, url_prefix="/news")
--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask_dir.app import app
-import flask_dir.view
-from flask_dir.papers.blueprint import papers as blueprint_paper
+from flask_dir import app
 
-
-app.register_blueprint(blueprint_paper, url_prefix="/news")
 
 if __name__ == '__main__':
     app.run()
