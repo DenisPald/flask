bbb93e938e6929975debb64a44d58fff544fc482
DenisPALD <masgutovmihail@gmail.com>
2021-07-14 19:52:41 +0500

    add tag to db

--- flask_dir/app.py
+++ flask_dir/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 
-with sqlite3.connect('data.db', check_same_thread=False) as con:
+with sqlite3.connect('data.db') as con:
     cur = con.cursor()
 
     cur.execute("""CREATE TABLE IF NOT EXISTS paper(
@@ -17,4 +17,17 @@ with sqlite3.connect('data.db', check_same_thread=False) as con:
         text TEXT NOT NULL,
         slug STRING(32) NOT NULL UNIQUE,
         tag TEXT
-        )""")
+        );""")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS tag (
+	    id INTEGER PRIMARY KEY AUTOINCREMENT,
+	    name STRING(32) NOT NULL UNIQUE,
+	    slug STRING(64) NOT NULL UNIQUE,
+        );""")
+
+    cur.execute("""CREATE TABLE IF NOT EXISTS paper_tag (
+	    id_paper INTEGER NOT NULL,
+	    id_tag INTEGER NOT NULL,
+	    FOREIGN KEY("id_tag") REFERENCES "tag"("id"),
+	    FOREIGN KEY("id_paper") REFERENCES "paper"("id")
+        );""")
